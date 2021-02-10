d2161ccc4b3ecf3bfb6bfc1714ec8dea92e4f712
Houssam <houssam.naous1@gmail.com>
2021-02-10 18:13:06 +0100

    added ability to draw lines

--- ARDrawing/ViewController.swift
+++ ARDrawing/ViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.session.run(configuration)
+        self.sceneView.debugOptions = [ ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.showsStatistics = true
+        self.sceneView.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var drawButton: UIButton!
 
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        print("rendering")
+        guard let pointView = sceneView.pointOfView else {return}
+        let transform = pointView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33) // m31 = ( row3 column1 )
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        
+        let currentPositionOfCamera = orientation + location
+        
+        DispatchQueue.main.async {
+            if(self.drawButton.isHighlighted) {
+                let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.02))
+                sphereNode.position = currentPositionOfCamera
+                self.sceneView.scene.rootNode.addChildNode(sphereNode)
+                sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+            }else{
+                // let pointer = SCNNode(geometry: SCNSphere(radius: 0.01))
+                // pointer.position = currentPositionOfCamera
+                
+            }
+        }
+        
+        
+    }
+    
+}
+
+func +(left: SCNVector3, right: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(left.x + right.y, left.y + right.y, left.z + right.z)
 }
 
